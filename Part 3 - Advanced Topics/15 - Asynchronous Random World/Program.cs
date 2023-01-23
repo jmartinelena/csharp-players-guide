d797295796741a1b9496172c3addec494c105e58
jmartinelena <jmartinelena1@gmail.com>
2023-01-23 00:18:39 -0300

    Added solutions to chapter 44's exercises.

---
+++ Part 3 - Advanced Topics/15 - Asynchronous Random World/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Type in a word that you want to randomly recreate: ");
+string word = Console.ReadLine() ?? "none";
+
+DateTime now = DateTime.Now;
+int count = await RandomlyRecreateAsync(word);
+DateTime after = DateTime.Now;
+
+Console.WriteLine($"It took {count} times and {(after-now).TotalSeconds:0.000} seconds to randomly recreate {word}.");
+
+int RandomlyRecreate(string word)
+{
+    word = word.ToLower();
+    Random random = new Random();
+    int attempts = 0;
+
+    while (true)
+    {
+        string randomWord = "";
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            char randomChar = (char)('a' + random.Next(26));
+            randomWord += randomChar;
+        }
+
+        attempts++;
+
+        if (randomWord == word) break;
+    }
+
+    return attempts;
+}
+
+Task<int> RandomlyRecreateAsync(string word)
+{
+    return Task.Run( () => RandomlyRecreate(word) );
+}
---
+++ Part 3 - Advanced Topics/16 - Many Random Worlds/Program.cs
@@ -0,0 +1,46 @@
+﻿while (true)
+{
+    Console.WriteLine("Type exit if you want to finish.");
+    Console.Write("Type in a word that you want to randomly recreate: ");
+    string word = Console.ReadLine() ?? "exit";
+    if (word == "exit") break;
+    GenerateWord(word);
+}
+
+async Task GenerateWord(string word)
+{
+    DateTime now = DateTime.Now;
+    int count = await RandomlyRecreateAsync(word);
+    DateTime after = DateTime.Now;
+
+    Console.WriteLine($"\nIt took {count} times and {(after - now).TotalSeconds:0.000} seconds to randomly recreate {word}.");
+}
+
+int RandomlyRecreate(string word)
+{
+    word = word.ToLower();
+    Random random = new Random();
+    int attempts = 0;
+
+    while (true)
+    {
+        string randomWord = "";
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            char randomChar = (char)('a' + random.Next(26));
+            randomWord += randomChar;
+        }
+
+        attempts++;
+
+        if (randomWord == word) break;
+    }
+
+    return attempts;
+}
+
+Task<int> RandomlyRecreateAsync(string word)
+{
+    return Task.Run(() => RandomlyRecreate(word));
+}
