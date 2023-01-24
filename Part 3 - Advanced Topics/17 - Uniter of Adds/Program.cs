f6f28c5578eb3cccb1f63e3a2143fd79ba640091
jmartinelena <jmartinelena1@gmail.com>
2023-01-23 21:11:06 -0300

    Added solutions to chapter 45's exercises.

---
+++ Part 3 - Advanced Topics/17 - Uniter of Adds/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine(Add(1, 2));
+Console.WriteLine(Add(2.0, 2.0));
+Console.WriteLine(Add("a", "bc"));
+Console.WriteLine(Add(DateTime.Now, new TimeSpan(0, 10, 10)));
+
+dynamic Add(dynamic a, dynamic b) => a + b;
---
+++ Part 3 - Advanced Topics/18 - The Robot Factory/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Dynamic;
+
+int count = 1;
+
+while (true)
+{
+    Console.WriteLine($"You are creating robot #{count}.");
+    dynamic robot = new ExpandoObject();
+    robot.ID = count;
+    count++;
+
+    // I don't like this method of getting the properties for the robot because it is too repetitive.
+    // I tried making a method with the robot's desired property as a parameter, and then assign a value to it
+    // within the method, but I couldn't get it to work.
+    // Reference: https://stackoverflow.com/questions/62401329/dynamic-properties-passed-as-c-sharp-method-parameters
+    Console.Write("Do you want to name the robot? ");
+    if (Console.ReadLine() != "no")
+    {
+        while (true)
+        {
+            Console.Write("What is it's name? ");
+            string? name = Console.ReadLine();
+            
+            if (name != null)
+            {
+                robot.Name = name;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid name, try again.");
+            }
+        }
+    }
+
+    Console.Write("Does this robot have a specific size? ");
+    if (Console.ReadLine() != "no")
+    {
+        while (true)
+        {
+            Console.Write("What is it's height? ");
+            string? height = Console.ReadLine();
+
+            if (int.TryParse(height, out int parsedHeight) && parsedHeight > 0)
+            {
+                robot.Height = parsedHeight;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid height, try again.");
+            }
+        }
+
+        while (true)
+        {
+            Console.Write("What is it's width? ");
+            string? width = Console.ReadLine();
+
+            if (int.TryParse(width, out int parsedWidth) && parsedWidth > 0)
+            {
+                robot.Width = parsedWidth;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid width, try again.");
+            }
+        }
+    }
+
+    Console.Write("Does this robot need to be a specific color? ");
+    if (Console.ReadLine() != "no")
+    {
+        while (true)
+        {
+            Console.Write("What is it's color? ");
+            string? color = Console.ReadLine();
+
+            if (color != null)
+            {
+                robot.Color = color;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid name, try again.");
+            }
+        }
+    }
+
+    foreach (KeyValuePair<string, object> property in (IDictionary<string, object>)robot)
+    {
+        Console.WriteLine($"{property.Key}: {property.Value}");
+    }
+}
