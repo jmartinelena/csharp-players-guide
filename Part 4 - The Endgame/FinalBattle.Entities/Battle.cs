16d82745bd4591ae6c8dff4ed29be4cd0018f84d
jmartinelena <jmartinelena1@gmail.com>
2023-01-25 17:50:04 -0300

    Did Core Game: Battle Series

--- Part 4 - The Endgame/FinalBattle.Entities/Battle.cs
+++ Part 4 - The Endgame/FinalBattle.Entities/Battle.cs
@@ -10,60 +10,77 @@ namespace FinalBattle
     public class Battle
     {
         public Party HeroParty { get; init; }
-        public Party EnemyParty { get; init; }
+        public List<Party> EnemyParties { get; }
+        public Party CurrentEnemyParty { get; set; }
         private bool _heroPartyTurn = true;
 
-        public Battle(Party heroParty, Party enemyParty)
+        public Battle(Party heroParty, List<Party> enemyParties)
         {
             HeroParty = heroParty;
-            EnemyParty = enemyParty;
+            EnemyParties = enemyParties;
+            CurrentEnemyParty = enemyParties[0];
         }
 
         public void Run()
         {
-            while (true)
+            for (int index = 0; index < EnemyParties.Count; index++)
             {
-                Party party = _heroPartyTurn ? HeroParty : EnemyParty;
-
-                foreach (Character character in party.Characters)
-                {
-                    Console.ForegroundColor = _heroPartyTurn ? ConsoleColor.Blue : ConsoleColor.Red;
-                    Console.WriteLine($"It is {character.Name}'s turn...");
-                    Console.ResetColor();
-
-                    Console.WriteLine(party.Player.ChooseAction(this, character).Run(this, character));
-                    Console.WriteLine();
-
-                    if (BattleIsOver) break;
-                }
-
-                if (HeroParty.Characters.Count == 0)
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("The heroes have lost, and the Uncoded One's forces have prevailed...");
-                    Console.ResetColor();
-                    break;
-                }
-                else if (EnemyParty.Characters.Count == 0)
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("The heroes have won, and the Uncoded One's evil reign is over!");
-                    Console.ResetColor();
-                    break;
-                }
+                _heroPartyTurn = true;
+                CurrentEnemyParty = EnemyParties[index];
                 
-                _heroPartyTurn = !_heroPartyTurn;
+                while (true)
+                {
+                    Party party = _heroPartyTurn ? HeroParty : CurrentEnemyParty;
+
+                    foreach (Character character in party.Characters)
+                    {
+                        Console.ForegroundColor = _heroPartyTurn ? ConsoleColor.Blue : ConsoleColor.Red;
+                        Console.WriteLine($"It is {character.Name}'s turn...");
+                        Console.ResetColor();
+
+                        Console.WriteLine(party.Player.ChooseAction(this, character).Run(this, character));
+                        Console.WriteLine();
+
+                        if (BattleIsOver) break;
+                    }
+
+                    if (HeroParty.Characters.Count == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("The heroes have lost, and the Uncoded One's forces have prevailed...");
+                        Console.ResetColor();
+                        break;
+                    }
+                    else if (CurrentEnemyParty.Characters.Count == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("The heroes have won this fight!\n--------------------------------------------\n");
+                        Console.ResetColor();
+                        break;
+                    }
+
+                    _heroPartyTurn = !_heroPartyTurn;
+                }
+
+                if (HeroParty.Characters.Count == 0) break;
+            }
+            
+            if (HeroParty.Characters.Count != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("The heroes have won, and the evil reign of the Uncoded One is no more!");
+                Console.ResetColor();
             }
         }
 
         public Party GetPartyFor(Character character)
         {
-            return HeroParty.Characters.Contains(character) ? HeroParty : EnemyParty;
+            return HeroParty.Characters.Contains(character) ? HeroParty : CurrentEnemyParty;
         }
         public Party GetEnemyPartyFor(Character character)
         {
-            return HeroParty.Characters.Contains(character) ? EnemyParty : HeroParty;
+            return HeroParty.Characters.Contains(character) ? CurrentEnemyParty : HeroParty;
         }
-        public bool BattleIsOver => HeroParty.Characters.Count == 0 || EnemyParty.Characters.Count == 0;
+        public bool BattleIsOver => HeroParty.Characters.Count == 0 || CurrentEnemyParty.Characters.Count == 0;
     }
 }
--- Part 4 - The Endgame/FinalBattle/Program.cs
+++ Part 4 - The Endgame/FinalBattle/Program.cs
@@ -9,7 +9,9 @@ string name = ConsoleHelper.Ask("True Programmer, what is your name?");
 TrueProgrammer trueProgrammer = new TrueProgrammer(name);
 
 Party heroParty = new Party(new ComputerPlayer(), new List<Character> { trueProgrammer });
-Party enemyParty = new Party(new ComputerPlayer(), new List<Character> { new Skeleton() });
+//Party enemyParty = new Party(new ComputerPlayer(), new List<Character> { new Skeleton() });
+List<Party> enemyParties = new List<Party> { new Party(new ComputerPlayer(), new List<Character> { new Skeleton() }),
+new Party(new ComputerPlayer(), new List<Character> { new Skeleton(), new Skeleton() }) };
 
-Battle battle = new Battle(heroParty, enemyParty);
+Battle battle = new Battle(heroParty, enemyParties);
 battle.Run();
